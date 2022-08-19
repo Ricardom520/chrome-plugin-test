5544e008784f0b6f8bc3355b0b15ab004ae5a6d6
tanridong <tanridong@yy.com>
2022-08-19 19:10:22 +0800

    test: 备份

--- js/test.js
+++ js/test.js
@@ -1,1 +1,24 @@
-alert("test")
+var YYLiveBear = (function() {
+  var obj = function() {
+    this.init()
+  }
+
+  obj.prototype = {
+    constructor: obj,
+
+    // 入口函数
+    init: function () {
+      // 创建全局dom 
+      var dom = document.createElement('div')
+      dom.id = 'yybear_pop'
+      console.log(document)
+      document.body.appendChild(dom)
+    }
+  }
+
+  return obj
+})()
+
+window.onload = function() {
+  new YYLiveBear()
+}

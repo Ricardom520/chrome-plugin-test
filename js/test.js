d245506f58b0cd0ff5061f4995002e9037425a88
tanridong <tanridong@yy.com>
2022-09-01 00:16:44 +0800

    test: 备份

--- js/test.js
+++ js/test.js
@@ -13,7 +13,7 @@ var YYLiveBear = (function() {
       dom.id = 'yybear_pop'
 
       var temp = function() {
-        return `<canvas id='yylive_bear' width='100%' height='100%'></canvas>`
+        return `<canvas id='yylive_bear'></canvas>`
       }
       console.log(document)
       dom.appendChild(this.createFrag(temp()))
@@ -28,16 +28,148 @@ var YYLiveBear = (function() {
 
     // html内容
     render: function() {
+      this.canvasRender()
+    },
+
+    // canvas画布
+    canvasRender: function() {
+      var gradientYellow = [
+        {
+          pro: 0,
+          color: '#FFE652'
+        },
+        {
+          pro: 1,
+          color: '#FCD21C'
+        }
+      ]
+
       // canvas画布
       var canvas = document.getElementById('yylive_bear')
       var ctx = canvas.getContext('2d')
 
-      // 一个圆
-      ctx.beginPath()
-      ctx.arc(0, 0, 50, 0, 2 * Math.PI)
-      ctx.stroke()
+      canvas.addEventListener('click', () => {
+        console.log('点击了')
+        parent.postMessage({
+          val: 'chrome插件测试'
+        }, 'https://www.yy.com')
+      })
 
+      // 画脸
+      bearFace()
+      function bearFace() {
+        ctx.save()
+        ctx.beginPath()
+        ctx.moveTo(20, 115)
+        ctx.bezierCurveTo(35, 140, 115, 140, 130, 115)
 
+        ctx.bezierCurveTo(130, 115, 155, 80, 90, 45)
+        ctx.bezierCurveTo(130, 160, 160, 20, 100, 52)
+        ctx.bezierCurveTo(100, 52, 95, 48, 90, 45)
+        
+        ctx.bezierCurveTo(88, 45, 95, 28, 70, 45)
+        ctx.bezierCurveTo(70, 45, 65, 48, 62, 50)
+        
+        ctx.bezierCurveTo(62, 50, 20, 20, 25, 78)
+        ctx.bezierCurveTo(25, 78, 50, 60, 70, 45)
+        
+        ctx.bezierCurveTo(70, 45, 0, 75, 20, 115)
+        fillGradientColor(ctx, 20, 115, 135, 135, gradientYellow)
+        ctx.closePath()
+        ctx.restore()
+      }
+
+      // 左耳
+      leftEar()
+      function leftEar() {
+        ctx.save()
+        ctx.beginPath()
+        ctx.moveTo(52, 55)
+        ctx.bezierCurveTo(52, 55, 30, 40, 35, 68)
+        fillColor(ctx, '#8D4F1F')
+        ctx.closePath()
+        ctx.restore()
+      }
+
+      // 右耳
+      rightEar()
+      function rightEar() {
+        ctx.save()
+        ctx.beginPath()
+        ctx.moveTo(125, 74)
+        ctx.bezierCurveTo(125, 74, 130, 50, 110, 58)
+        fillColor(ctx, '#8D4F1F')
+        ctx.closePath()
+        ctx.restore()
+      }
+
+      // 熊猫眼
+      eyeBgColor()
+      function eyeBgColor() {
+        ctx.save()
+        ctx.beginPath()
+        ctx.moveTo(78, 80)
+        ctx.bezierCurveTo(70, 55, 40, 65, 35, 95)
+        ctx.bezierCurveTo(35, 95, 30, 122, 60, 110)
+        ctx.bezierCurveTo(60, 110, 80, 95, 100, 110)
+        ctx.bezierCurveTo(100, 110, 125, 125, 122, 96)
+        ctx.bezierCurveTo(122, 90, 106, 45, 87, 80)
+        ctx.bezierCurveTo(87, 80, 84, 85, 78, 80)
+        ctx.closePath()
+        fillColor(ctx, '#8D4F1F')
+        ctx.restore()
+      }
+
+      // 左眼
+      leftEye()
+      function leftEye() {
+        ellipseOne(ctx, 60, 88, 10, 13, '#FFF')
+        ellipseOne(ctx, 61, 88, 8, 10, '#8D4F1F')
+        ellipseOne(ctx, 64, 85, 3, 4, '#fff')
+      }
+
+      // 右眼
+      rightEye()
+      function rightEye() {
+        ellipseOne(ctx, 102, 89, 10, 13, '#FFF')
+        ellipseOne(ctx, 102, 89, 8, 10, '#8D4F1F')
+        ellipseOne(ctx, 104, 87, 3, 4, '#fff')
+      }
+
+      // 鼻子
+      mouse()
+      function mouse() {
+        
+      }
+      
+      // 填充过渡颜色
+      function fillGradientColor(context, x, y, r1, r2, options) {
+        var gradient = context.createLinearGradient(x, y, r1, r2)
+        for (var i = 0; i < options.length; i++) {
+          gradient.addColorStop(options[i].pro, options[i].color)
+        }
+
+        context.fillStyle = gradient
+        context.fill()
+      }
+
+      // 一个椭圆
+      function ellipseOne(context , x, y, a, b, color) {
+        var step = (a > b) ? 1 / a : 1 / b
+        context.beginPath()
+        context.moveTo(x + a, y)
+        for(var i = 0; i < 2 * Math.PI; i += step) {
+            context.lineTo(x + a * Math.cos(i), y + b * Math.sin(i))
+        }
+        context.closePath()
+        fillColor(context, color)
+      }
+
+      // 填充颜色
+      function fillColor(context, val) {
+        context.fillStyle = val
+        context.fill()
+      }
     }
   }
 
